fa6f5a43aaf9ea68f0d35727578bc360b82fdf11
vanya andreychuk <vandreychyk@gmail.com>
2018-03-02 19:16:38 +0200

    - test

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 cd ./example
 docker-compose -f docker-compose.yml up -d --build
 cd ../
-newman run --bail --ignore-redirects --global-var host=localhost ./postgrest-oauth-server.postman_collection.json
+newman run ./postgrest-oauth-server.postman_collection.json --bail --ignore-redirects --global-var host=localhost
