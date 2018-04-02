6a882101c8639a298a5bb3a2fe2c3ecb51d89d7c
vanya andreychuk <vandreychyk@gmail.com>
2018-04-02 21:25:33 +0300

    - add spleep 5 to test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@
 cd ./example
 docker-compose -f docker-compose.yml up -d --build
 cd ../
+sleep 5
 newman run ./postgrest-oauth-server.postman_collection.json --bail --ignore-redirects --global-var host=localhost
