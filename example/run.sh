84f54ddad47398a2df334869a187eab9af236215
Ivan Kuznetsov <kuzma.wm@gmail.com>
2018-05-08 20:13:32 +0300

    Remove example folder in favour of separate repository

--- example/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose.yml stop
-docker-compose -f docker-compose.yml rm -f
-docker volume ls -q | xargs docker volume rm
-docker-compose -f docker-compose.yml up --build    
