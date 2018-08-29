9ef2351d940e35a5cc694bf34353a5f7184610b1
Ivan Kuznetsov <kuzma.wm@gmail.com>
2018-08-29 19:53:19 +0300

    Fix CORS OPTIONS passthrough

--- main.go
+++ main.go
@@ -14,7 +14,16 @@ var Router = mux.NewRouter().StrictSlash(true)
 
 func main() {
 	log.Println("Started!")
-	corsRouter := cors.AllowAll().Handler(Router)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+		OptionsPassthrough: true,
+	})
+
+	corsRouter := c.Handler(Router)
 	loggedRouter := handlers.LoggingHandler(os.Stdout, corsRouter)
 	log.Fatal(http.ListenAndServe(":3684", loggedRouter))
 }
