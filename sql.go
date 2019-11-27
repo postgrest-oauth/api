ea14f0c7468f03762a3ae950951855db4d2ae558
Ivan Kuznetsov <kuzma.wm@gmail.com>
2019-11-27 18:36:09 +0200

    Add signup data

--- sql.go
+++ sql.go
@@ -15,7 +15,7 @@ type Owner struct {
 	Password          string
 	Email             string
 	Phone             string
-	Language          string
+	Data              string
 	VerificationCode  string
 	VerificationRoute string
 }
@@ -35,7 +35,7 @@ func (a *Owner) create() (id string, err error) {
 	db, err := dbConnect()
 	defer db.Close()
 	query := fmt.Sprintf("SELECT id::varchar FROM oauth2.create_owner('%s', '%s', '%s', '%s', '%s', '%s')",
-		a.Email, a.Phone, a.Password, a.Language, a.VerificationCode, a.VerificationRoute)
+		a.Email, a.Phone, a.Password, a.Data, a.VerificationCode, a.VerificationRoute)
 	err = db.QueryRow(query).Scan(&id)
 
 	switch {
