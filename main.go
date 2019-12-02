e13d4cf3006890669150ee209e07b4102e9fe2a5
Ivan Kuznetsov <kuzma.wm@gmail.com>
2019-12-02 22:13:53 +0200

    Better CORS security
    https://github.com/rs/cors/issues/55

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/caarlos0/env"
 	"log"
 	"net/http"
 	"os"
@@ -12,9 +13,23 @@ import (
 
 var Router = mux.NewRouter().StrictSlash(true)
 
+var authCodeConfig struct {
+	ValidateRedirectURI bool   `env:"OAUTH_VALIDATE_REDIRECT_URI" envDefault:"true"`
+	OauthCodeUi         string `env:"OAUTH_CODE_UI" envDefault:"http://localhost:3685"`
+}
+
+func init() {
+	err := env.Parse(&authCodeConfig)
+	if err != nil {
+		log.Printf("%+v\n", err)
+	}
+}
+
 func main() {
 	log.Println("Started!")
-	corsRouter := cors.AllowAll().Handler(Router)
+	corsRouter := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://foo.com"},
+	}).Handler(Router)
 	loggedRouter := handlers.LoggingHandler(os.Stdout, corsRouter)
 	log.Fatal(http.ListenAndServe(":3684", loggedRouter))
 }
